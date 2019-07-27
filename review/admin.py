78d8e68961ef00d52a69336c5908bcede0962b06
Nonso <nonsookoroafor@gmail.com>
2019-07-27 22:31:41 +0100

    admin site

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+import review.models as rm
 
 # Register your models here.
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'street_name', 'house_number', 'location', 'review', 'image', 'created', )
+
+
+admin.site.register(rm.Review, ReviewAdmin)
+
